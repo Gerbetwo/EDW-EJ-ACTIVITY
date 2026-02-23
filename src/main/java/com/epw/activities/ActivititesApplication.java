52555b809877217f1ae6904962b5d4413f75f9ee
Gerbetwo <lassomoluna9@gmail.com>
2026-02-23 12:49:22 -0500

    fix: full working ativities

--- src/main/java/com/epw/activities/ActivititesApplication.java
+++ src/main/java/com/epw/activities/ActivititesApplication.java
@@ -1,4 +1,4 @@
-package com.example.activitites;
+package com.epw.activities;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
