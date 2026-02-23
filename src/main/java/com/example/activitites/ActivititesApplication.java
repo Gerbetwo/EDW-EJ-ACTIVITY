8fa1614b94741e01defe146981b62b7a359c6f0d
Gerbetwo <lassomoluna9@gmail.com>
2026-02-23 12:16:28 -0500

    Feat: First Commit

---
+++ src/main/java/com/example/activitites/ActivititesApplication.java
@@ -0,0 +1,13 @@
+package com.example.activitites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivititesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivititesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/activitites/ActivititesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activitites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivititesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
